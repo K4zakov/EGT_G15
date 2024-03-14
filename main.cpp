4b5f0303b039f16945e96d39a54735090d646218
Roberto <156944367+K4zakov@users.noreply.github.com>
2024-03-14 15:39:31 +0200

    Revert "int argc, char argv[} test"
    This reverts commit a978277b2957943263d796b61d9411c33262ed12.

--- main.cpp
+++ main.cpp
@@ -9,18 +9,6 @@
 int main( int argc, char *argv[]) 
 {
 
-	std::cout << "argc = " << argc << std::endl;
-    std::cout << "Lets see what is in argv[]" << std::endl;
-
-    for (int i = 1; i < argc; i++) {
-        std::cout << "argv[" << i << "] = " << argv[i] << std::endl;
-    }
-	// printf("argc =  %d\n", argc);
-	// printf("Lets see what is in argv[]\n");
-
-	// for(int i = 1; i < argc; i++){
-	// 	printf("argv[%d] = %s\n", i, argv[i]);
-	// }
 	Game15 game= Game15();
 	Game15BoardView view= Game15BoardView(game);
 	view.RedrawAll();
